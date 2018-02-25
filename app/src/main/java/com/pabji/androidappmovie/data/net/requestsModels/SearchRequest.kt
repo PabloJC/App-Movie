9be70b887631f974c4639550c92d6341d497f243
Pablo Jimenez Casado <pjimenez.casado@atsistemas.com>
2018-02-25 12:53:23 +0100

    Add SearchRequest

--- app/src/main/java/com/pabji/androidappmovie/data/net/requestsModels/SearchRequest.kt
+++ app/src/main/java/com/pabji/androidappmovie/data/net/requestsModels/SearchRequest.kt
@@ -1,9 +1,21 @@
 package com.pabji.androidappmovie.data.net.requestsModels
 
-import android.provider.Settings.Secure.getString
-import com.pabji.androidappmovie.R
 
-class SearchRequest{
+data class SearchRequest(var api_key : String, var language : String? = "es", var page : Int? = 1){
 
-    
+
+    fun toQueryMap() : Map<String,String> {
+        val data = hashMapOf<String, String>()
+        data["api_key"] = api_key
+
+        language?.let {
+            data["language"] = it
+        }
+
+        page?.let {
+            data["page"] = it.toString()
+        }
+
+        return data
+    }
 }
